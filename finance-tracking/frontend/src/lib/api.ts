260aa2944761b25b46f9af866798b7e1b5e19b2d
tarunguduru2811 <127959942+tarunguduru2811@users.noreply.github.com>
2026-01-21 15:56:08 +0530

    Update baseURL for API configuration

--- finance-tracking/frontend/src/lib/api.ts
+++ finance-tracking/frontend/src/lib/api.ts
@@ -3,7 +3,7 @@ import { config } from "process";
 
 
 export const api = axios.create({
-    baseURL:`${process.env.NEXT_PUBLIC_API_URL}/api`,
+    baseURL:process.env.NEXT_PUBLIC_API_URL,
     withCredentials:true
 })
 
