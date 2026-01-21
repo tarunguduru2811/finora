fc0ccc7c9ddbb9458b9527884ce7b2732d84d842
tarunguduru2811 <127959942+tarunguduru2811@users.noreply.github.com>
2026-01-21 12:34:39 +0530

    Update API base URL to include '/api' suffix

--- finance-tracking/frontend/src/lib/api.ts
+++ finance-tracking/frontend/src/lib/api.ts
@@ -3,7 +3,7 @@ import { config } from "process";
 
 
 export const api = axios.create({
-    baseURL:process.env.NEXT_PUBLIC_API_URL,
+    baseURL:`${process.env.NEXT_PUBLIC_API_URL}/api`,
     withCredentials:true
 })
 
