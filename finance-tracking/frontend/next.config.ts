ca150c035d658b1edd4f08f1b714d9e2e5b0190c
tarunguduru2811 <127959942+tarunguduru2811@users.noreply.github.com>
2026-01-29 11:09:11 +0530

    Add API rewrites for external destination

--- finance-tracking/frontend/next.config.ts
+++ finance-tracking/frontend/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["sdmntprcentralus.oaiusercontent.com"], // add your domains here
   },
+   async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://finora-1-k8mq.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
